77839338dd579ca1b89d4f9ea281fd3c0e43ab1b
traderspostcom <russdjohnson@gmail.com>
2025-09-22 11:04:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,20 @@
+export default {
+  scoring: {
+    RLM: 2,
+    STEAM: 2,
+    KEYNUM: 1.5,
+    LATE: 1.5,
+    OUTLIER: 1,
+    SPLIT: 1
+  },
+  thresholds: {
+    strong: 5,
+    lean: 3
+  },
+  reAlert: {
+    cooldownMinutes: 20,
+    expiryMinutes: 240
+  },
+  stateFile: "./sharp_state.json"
+};
 
